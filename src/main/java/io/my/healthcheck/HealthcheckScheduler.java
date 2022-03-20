4f072ecd6d62ff64c1644dfade40e287fd3bf7c7
mysend12 <mysend12@naver.com>
2022-03-20 19:43:47 +0900

    Healthcheck 스케줄링 추가(500 방지)

--- src/main/java/io/my/healthcheck/HealthcheckScheduler.java
+++ src/main/java/io/my/healthcheck/HealthcheckScheduler.java
@@ -1,20 +1,20 @@
-//package io.my.healthcheck;
-//
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class HealthcheckScheduler {
-//    private final HealthcheckService healthcheckService;
-//
-//    public HealthcheckScheduler(HealthcheckService healthcheckService) {
-//        this.healthcheckService = healthcheckService;
-//    }
-//
-//    @Scheduled(fixedDelay = 1000 * 60 * 4)
-//    public void cronJobSch() {
-//        healthcheckService.healthcheck();
-//    }
-//
-//
-//}
+package io.my.healthcheck;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthcheckScheduler {
+    private final HealthcheckService healthcheckService;
+
+    public HealthcheckScheduler(HealthcheckService healthcheckService) {
+        this.healthcheckService = healthcheckService;
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60 * 2)
+    public void cronJobSch() {
+        healthcheckService.healthcheck();
+    }
+
+
+}
